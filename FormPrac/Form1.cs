bf08b015057d0556015cb96b8368938cc1731421
Sookhee <s2018s38@e-mirim.hs.kr>
2020-06-17 14:03:13 +0900

    초기 커밋.

---
+++ FormPrac/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormPrac
+{
+    public partial class Form1 : Form
+    {
+        class CustomForm : Form{
+            public CustomForm()
+            {
+                this.Text = "내가 만든 폼!";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            IsMdiContainer = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CustomForm cf = new CustomForm();
+            cf.MdiParent = this;
+            cf.Show();
+        }
+    }
+}
